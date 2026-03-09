7fff69210aaf64318ab2faeb26d300c2b7a3a391
MohammadMasumHossain <masumhossainrana71@gmail.com>
2026-03-09 14:35:09 +0600

    feat(user): implementation user creation API

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
 import jobRouter from "./routes/jobRoutes";
+import userRoutes from "./routes/userRoutes";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/", jobRouter);
+app.use("/", userRoutes);
 
 export default app;
---
+++ Server/src/controllers/userController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { db } from "../config/db";
+import { ObjectId } from "mongodb";
+
+const userCollection = db.collection("users");
+
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const user = req.body;
+
+    const result = await userCollection.insertOne(user);
+
+    res.status(201).json({
+      message: "User created",
+      id: result.insertedId,
+    });
+  } catch {
+    res.status(500).json({ error: "Create user failed" });
+  }
+};
