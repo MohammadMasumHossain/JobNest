12c9ff66ed11810582aa2c625fef5c2af0287e10
MohammadMasumHossain <masumhossainrana71@gmail.com>
2026-03-09 13:47:28 +0600

    feat(job): add update job API

--- Server/src/controllers/jobController.ts
+++ Server/src/controllers/jobController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { db } from "../config/db";
 import { ObjectId } from "mongodb";
+import { Job } from "../types/Job";
 
 const jobCollection = db.collection("jobs");
 
@@ -24,4 +25,23 @@ export const createJob = async (req: Request, res: Response) => {
 export const getJobs = async (_req: Request, res: Response) => {
   const jobs = await jobCollection.find().toArray();
   res.json(jobs);
+};
+
+export const updateJob = async (
+  req: Request<{ id: string }, {}, Partial<Job>>,
+  res: Response,
+) => {
+  try {
+    const id = req.params.id;
+
+    // Use req.body directly as update data
+    await jobCollection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: req.body }, // no destructuring needed
+    );
+
+    res.json({ message: "Job updated" });
+  } catch (err) {
+    res.status(500).json({ error: "Update failed" });
+  }
 };
--- Server/src/routes/jobRoutes.ts
+++ Server/src/routes/jobRoutes.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { createJob, getJobs } from "../controllers/jobController";
+import { createJob, getJobs, updateJob } from "../controllers/jobController";
 
 const router = express.Router();
 router.post("/job", createJob);
 router.get("/jobs", getJobs);
+router.put("/job/:id", updateJob);
 export default router;
